9464309ac1a656ac9d3b8bc923e79849e033fc3e
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-03-31 20:15:55 +0900

    [Add] #main - UIColor+ 분리

---
+++ 1차 세미나 실습/34th sopt 1th Seminar/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  34th sopt 1th Seminar
+//
+//  Created by 이명진 on 3/31/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let daangunOrange = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+    static let daangunGray = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+}
