aae0dc0641c51ebcfa0900649b41eebf456b5861
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-04-20 14:52:20 +0900

    [Setting] #main - set project

---
+++ 세미나 실습/34th sopt 1th Seminar/3weeks/ChatView.swift
@@ -0,0 +1,8 @@
+//
+//  ChatView.swift
+//  34th sopt 1th Seminar
+//
+//  Created by 이명진 on 4/20/24.
+//
+
+import Foundation
---
+++ 세미나 실습/34th sopt 1th Seminar/3weeks/ChatViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ChatViewController.swift
+//  34th sopt 1th Seminar
+//
+//  Created by 이명진 on 4/20/24.
+//
+
+import Foundation
+
+import UIKit
+
+final class ChatViewController: UIViewController {
+    
+    // MARK: - UIComponents
+    
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
+        setRegister()
+    }
+    
+    
+    // MARK: - UI & Layout
+    
+    private func setUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func setHierarchy() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    private func setDelegate() {
+        
+    }
+    
+    private func setRegister() {
+        
+    }
+    
+}
